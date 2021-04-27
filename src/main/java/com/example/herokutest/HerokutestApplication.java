2fcf31cd70477c7eeef7d2b15b664dd38662790d
Pierpaolo Di Dato <pierpaolo.pdd@gmail.com>
2021-04-27 14:38:08 +0200

    Update main class

--- src/main/java/com/example/herokutest/HerokutestApplication.java
+++ src/main/java/com/example/herokutest/HerokutestApplication.java
@@ -1,13 +1,23 @@
 package com.example.herokutest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HerokutestApplication {
+public class HerokutestApplication implements CommandLineRunner {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HerokutestApplication.class.getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(HerokutestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Prima applicazione deployata su Heroku con connessione al db");
 	}
 
 }
